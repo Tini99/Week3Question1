452fff0f5a95e46c300153a4d558f46f0c8b3fb4
Tini Gisa <1814759@nzse.ac.nz>
2021-06-20 16:24:47 +1200

    Add project files.

---
+++ Week3Question1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Week3Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+          Declair and  initialize array of 10 integers.
+          print only the odd numbers.
+            */
+            int[] array = new int[10];
+            int i;
+            for (i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Enter a array" + i);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                {
+                    if (array[i] % 2 != 0) ;
+                    {
+                        Console.WriteLine("");
+                    }
+                }
+            }
+        }
+    }
+}
